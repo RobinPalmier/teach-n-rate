353c25c8615880eed12e73a45df57ee278e7370d
mhossain <m.hossain@blackcover.fr>
2020-01-30 11:35:10 +0100

    feat: add express mongoose bodyparder and nodemon dependencies #9

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-const http = require('http');
+const express = require('express');
+
+const app = express();
 
 const hostname = '0.0.0.0';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
+app.get('/', (req, res) => {
+    res.status(200);
+    res.end('Hello this is Teach & Rate !');
+})
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+
+app.listen(port, hostname);
